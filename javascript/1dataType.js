a56527bc583f857bc46aea935401d4d46b528c17
aakashdeveloper <ahanda205@gmail.com>
2021-04-10 09:51:24 +0530

    bs4

---
+++ javascript/1dataType.js
@@ -0,0 +1,33 @@
+string > "dgd" "436" "dfgwer323" '35cf34'
+number >446 46464575 4.446 .45547
+boolean > true false
+
+es > ecma script 
+es5
+es6
+
+//es5
+var a = 10
+var b= "Hi"
+var c = true
+undefined
+a
+10
+b
+"Hi"
+c
+true
+typeof(a)
+"number"
+typeof(b)
+"string"
+typeof(c)
+"boolean"
+var d = "10"
+undefined
+typeof(d)
+"string"
+
+
+//es6
+let a = 10
---
+++ javascript/2concat.js
@@ -0,0 +1,17 @@
+//es5
+var a = "Hi"
+var b = "JavaScript"
+undefined
+a+b
+"HiJavaScript"
+var age = "10"
+var out = "my age is "+age
+undefined
+out
+"my age is 10"
+
+
+
+var age = "10"
+var out = `my age is ${age}`
+"my age is 10"
